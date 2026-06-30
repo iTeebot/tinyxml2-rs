ea3f68709260a271fb28eec25e0e6d864c39e1c0
AteebNoOne <ateebnoone@gmail.com>
2026-07-01 04:38:54 +0500

    Bundle C++ reference sources in tinyxml2-bench for packaging

--- crates/tinyxml2-bench/build.rs
+++ crates/tinyxml2-bench/build.rs
@@ -1,11 +1,11 @@
 fn main() {
     cc::Build::new()
         .cpp(true)
-        .file("../../include/tinyxml2.cpp")
+        .file("cpp/tinyxml2.cpp")
         .file("src/cpp_wrapper.cpp")
-        .include("../../include")
+        .include("cpp")
         .compile("tinyxml2_cpp_bench");
-    println!("cargo:rerun-if-changed=../../include/tinyxml2.cpp");
-    println!("cargo:rerun-if-changed=../../include/tinyxml2.h");
+    println!("cargo:rerun-if-changed=cpp/tinyxml2.cpp");
+    println!("cargo:rerun-if-changed=cpp/tinyxml2.h");
     println!("cargo:rerun-if-changed=src/cpp_wrapper.cpp");
 }
